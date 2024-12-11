11c352bf5a120579462890068ce94cb6ed959271
Mix Sirapat <sirapat.jr@gmail.com>
2024-12-11 16:50:21 +0900

    remove unused state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import { CustomCalendar } from '@/components/CustomCalendar';
 
 export default function Dashboard() {
-  const [date, setDate] = useState<Date | undefined>(new Date());
-
   return (
     <>
       <div className='flex items-center justify-items-center'>
