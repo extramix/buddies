c3ce7b3f83d357853be48bef366f95ab72678daf
Mix Sirapat <sirapat.jr@gmail.com>
2024-12-11 14:52:44 +0900

    add simple calendar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,23 @@
+//TODO: try to move state to a separate component
+'use client';
+import { Calendar } from '@/components/ui/calendar';
+import { useState } from 'react';
+
 export default function Dashboard() {
-  return <>Dashboard</>;
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  return (
+    <>
+      <div className='flex items-center justify-items-center'>
+        <main>
+          <Calendar
+            mode='single'
+            selected={date}
+            onSelect={setDate}
+            className='rounded-md border'
+          />
+        </main>
+      </div>
+    </>
+  );
 }
