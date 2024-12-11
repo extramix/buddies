ff51b0dc5ab82c0f7eafb94ee5c116660e1b1173
Mix Sirapat <sirapat.jr@gmail.com>
2024-12-11 16:52:59 +0900

    add json-sever package for mock data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,15 @@
 import { CustomCalendar } from '@/components/CustomCalendar';
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const data = await fetch(`http://localhost:8888/transactions`).then((res) =>
+    res.json()
+  );
+
   return (
     <>
       <div className='flex items-center justify-items-center'>
         <main>
+          {JSON.stringify(data)}
           <CustomCalendar />
         </main>
       </div>
