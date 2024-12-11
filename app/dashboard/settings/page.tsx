580e63fcd4c71128ae41f9b88d156c8d7d7edbdc
Mix Sirapat <sirapat.jr@gmail.com>
2024-12-11 14:55:42 +0900

    add settings page

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Settings() {
+  return (
+    <>
+      <div className='flex items-center justify-items-center'>
+        <main>Settings</main>
+      </div>
+    </>
+  );
+}
--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -15,12 +15,12 @@ import {
 const items = [
   {
     title: 'Home',
-    url: '#',
+    url: '/dashboard',
     icon: Home,
   },
   {
     title: 'Settings',
-    url: '#',
+    url: '/dashboard/settings',
     icon: Settings,
   },
 ];
