34579ed2ffd01d78cbd15332b47d726ddb57fcab
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-03-16 10:56:40 +0900

    refactor sidebar UI

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,22 @@
 // import { AppSidebar } from '@/components/AppSidebar';
 // import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
+import { AppSidebar } from "@/components/AppSidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    // <SidebarProvider>
-    //   <AppSidebar />
-    <main>
-      {/* <SidebarTrigger /> */}
-      {children}
-    </main>
-    // </SidebarProvider>
+    <SidebarProvider defaultOpen={false} 
+      style={{ 
+        "--sidebar-width": "14rem", 
+        "--sidebar-width-icon": "3rem" 
+      } as React.CSSProperties}>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="p-4">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
